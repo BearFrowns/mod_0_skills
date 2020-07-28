4d1406a05bc3f8d9d131b25ea23e55384f71c21b
Plum <plummer.gv@gmail.com>
2020-07-28 13:45:17 -0600

    Update variables.txt

---
+++ variables.js
@@ -0,0 +1,16 @@
+var food = "Fries"
+var is_delicious = true;
+var food = "Rice"
+var num2 = 2
+var num7 = 7
+
+function divide(num7, num2) {
+  return num7 / num2;
+}
+ var quotient = console.log(divide(num7, num2));
+
+var songs = ["Bohemian Rhapsody", "Rocketman", "Back in Black"]
+
+songs.shift();
+
+console.log("How many characters are there in the food string?"); console.log("There are " + food.length + " characters in the food string.");
